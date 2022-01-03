49b15687de22f3289a1a04f9d91604c50613cbc1
souravdas2015 <81551338+souravdas2015@users.noreply.github.com>
2022-01-03 21:40:16 +0530

    menu modification with name

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Dashboard } from './DashBoard/Dashboard';
-export { default as DataExtracter } from './DataExtrator/data-extract';
+export { default as Welcome } from './Welcome/Welcome';
 export { default as Stepper } from './Stepper/Stepper';
 export { default as TabMenu } from './TabMenu/TabMenu';
 export { default as Application } from './Application/Application';
